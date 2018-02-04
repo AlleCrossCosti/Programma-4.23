641efa88e3f9e0299e4428ff3069557fc605de55
Alle Cross Costi <costialle2@hotmail.it>
2018-02-04 07:44:03 +0100

    Initial commit

---
+++ Programma 4.23.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    
+    int num, risultato;
+    
+    num = 144;
+    risultato = sqrt(num);
+    printf("sqrt(%d)= %d", num, risultato);
+    
+    return 0;
+    
+    }
